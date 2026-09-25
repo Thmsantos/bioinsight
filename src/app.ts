c652667b988732bec295674fd453d0308d870e48
Thmsantos <thmessiasdev@gmail.com>
2026-09-25 16:03:28 -0300

    :wrench: config: config cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
+import { authRoutes, userRoutes } from "./api/modules/entities/user/routes/routes.ts";
+import { connectDb } from "./api/db/connection.ts";
+import cors from "@fastify/cors"; 
 import Fastify from "fastify";
 import type { FastifyInstance } from "fastify";
-import { connectDb } from "./api/db/connection.ts";
-import { authRoutes, userRoutes } from "./api/modules/entities/user/routes/routes.ts";
-import multipart from "@fastify/multipart";
 import { insightRoutes } from "./api/modules/entities/insight/routes/index.ts";
+import multipart from "@fastify/multipart";
 
 async function appBuild(): Promise<FastifyInstance> {
   await connectDb();
   const app = Fastify({ logger: true });
+
+  await app.register(cors, {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  });
 
   await app.register(multipart, {
     limits: { fileSize: 10 * 1024 * 1024 }
